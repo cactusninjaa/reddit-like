d0e302805bb00a3b9a0df06677494a379c0b4f56
Raphaël Chiche <raphael.ch2004@gmail.com>
2025-10-15 14:02:31 +0200

    fix: update Vercel configuration to use the correct path for builds and routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes.js";
-import AuthUser from "./models/authUsersModels.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
+
 const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
   throw new Error("MONGO_URI environment variable is not defined");
@@ -16,17 +16,19 @@ mongoose
   .connect(mongoUri)
   .then(() => console.log("✅ Connecté à MongoDB Atlas"))
   .catch((err) => console.error("❌ Erreur MongoDB:", err));
-const PORT = process.env.PORT;
+
+
 // Middleware pour parser le JSON
 app.use(express.json());
 
 // Utiliser les routes
 app.use("/api", router);
 
-app.listen(PORT, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on https://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+}
 export default app;
