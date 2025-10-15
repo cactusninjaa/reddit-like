4a73678d4680d2b064db478b1b00dcb92d9e5773
Thomas Sauvage <thomas.sauvage@edu.devinci.fr>
2025-10-15 10:24:31 +0200

    fix/index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,25 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import router from "./routes.js"
-
-dotenv.config()
-const app = express()
-app.use(cors())
-
-const PORT = process.env.PORT || 3000
-
+dotenv.config();
+const app = express();
+app.use(cors());
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+    throw new Error("MONGO_URI environment variable is not defined");
+}
+mongoose
+    .connect(mongoUri)
+    .then(() => console.log("✅ Connecté à MongoDB Atlas"))
+    .catch((err) => console.error("❌ Erreur MongoDB:", err)); 
+const PORT = process.env.PORT;
 app.use('/api', router);
-
-app.listen(PORT, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on https://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(server is listening on https://localhost:${PORT});
 });
-
-export default app
+export default app;
