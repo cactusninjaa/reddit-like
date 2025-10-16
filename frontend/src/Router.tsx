6d6f1e294de4627904a0c3322d7b337a24b315ad
Raphaël Chiche <raphael.ch2004@gmail.com>
2025-10-16 11:37:08 +0200

    feat: add user profile page with data fetching and styling

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import App from "./pages/App"
+import ProfilePage from "./pages/profile/[slug]/Page";
 
 const Router = () => {
     return (
@@ -9,6 +10,7 @@ const Router = () => {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/profile/:slug" element={<ProfilePage />} />
                 <Route path="*" element={<App />} />
             </Routes>
         </BrowserRouter>
