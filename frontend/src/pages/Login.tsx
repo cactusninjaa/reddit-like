34ff8945b8ecf3ffe8526e4c9b6398082a3dcba2
Raphaël Chiche <raphael.ch2004@gmail.com>
2025-10-16 10:04:01 +0200

    refactor: remove unused useEffect import from Login component

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router";
 import { loginUser } from "../api/api";
 import "../App.css";
